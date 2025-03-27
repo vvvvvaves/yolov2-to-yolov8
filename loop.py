202b4bf7d23e0a3ce0a5312db796f8b5573fbdb6
unknown <shkurenkov18@gmail.com>
2025-03-27 07:58:59 +0100

    Backbone experiments (focus)

--- loop.py
+++ loop.py
@@ -5,80 +5,68 @@ from torch.utils.data import DataLoader
 def training_loop(optimizer: torch.optim.Optimizer,
                   model,
                   loss_fn: torch.nn.Module,
-                  train_loader: DataLoader[tuple[torch.Tensor, torch.Tensor]],
-                  scaler) -> None:
+                  train_loader: DataLoader[tuple[torch.Tensor, torch.Tensor]]) -> None:
     model.train()
     loss_train = 0.0
+    correct = 0
+    total = 0
     for i, (imgs, labels) in enumerate(train_loader):
-        if (i+1) % 30 == 0:
+        imgs = imgs.to('cuda:0')
+        labels = labels.to('cuda:0')
+        
+        if (i+1) % 15 == 0:
             _datetime = datetime.datetime.now()
             print(f"{_datetime} Batch {i+1} ")
-
-        # ====================
-        # SCALER
-
-        with torch.amp.autocast(device_type="cuda", dtype=torch.float16):
-            optimizer.zero_grad()
-
-            out = model(imgs)
-
-            loss = loss_fn(out, labels)
-    
-        scaler.scale(loss).backward()
-
-        scaler.unscale_(optimizer)
-
-        # Since the gradients of optimizer's assigned params are unscaled, clips as usual:
-        # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
         
-        scaler.step(optimizer)
+        outputs = model(imgs)
 
-        scaler.update()
-
-        # ==========================
-        # NO SCALER
-
-        # outputs = model(imgs)
-
-        # loss = loss_fn(outputs, labels)
+        loss = loss_fn(outputs, labels)
         
-        # optimizer.zero_grad()
+        optimizer.zero_grad()
 
-        # loss.backward()
+        loss.backward()
 
-        # optimizer.step()
-
-        #=============================
+        optimizer.step()
 
         loss_train += loss.item()
-
-        del imgs, labels, out
+        preds = outputs.max(1)[1]
+        correct += preds.eq(labels).sum().item()
+        total += labels.shape[0]
 
     n_batches = len(train_loader)
+    accuracy = round(correct / total * 100., 4)
     loss_per_batch = round(loss_train / n_batches, 4)
-    print(f'[Train] Loss per batch: {loss_per_batch}')
-    return 0, loss_per_batch
+    print(f'[Train] Accuracy: {accuracy}%, Loss per batch: {loss_per_batch}')
+    return accuracy, loss_per_batch
 
 def validation_loop(model,
                     val_loader: DataLoader[tuple[torch.Tensor, torch.Tensor]],
                     loss_fn: torch.nn.Module) -> list[float]:
 
     model.eval()
-    loss_val = 0.0
+
+    correct = 0
+    total = 0
+    loss = 0.0
     with torch.no_grad():
         for i, (imgs, labels) in enumerate(val_loader):
+            imgs = imgs.to('cuda:0')
+            labels = labels.to('cuda:0')
             
-            if (i+1) % 30 == 0:
+            if (i+1) % 15 == 0:
                 _datetime = datetime.datetime.now()
                 print(f"{_datetime} Batch {i+1} ")
 
-            with torch.amp.autocast("cuda"):
-                out = model(imgs)
-                loss = loss_fn(out, labels)
-            loss_val += loss.item()
-            del imgs, labels, out
+            outputs = model(imgs)
+            preds = outputs.max(1)[1]
 
-    loss_per_batch = round(loss_val / len(val_loader), 4)
-    print(f"[Val]"
+            total += labels.shape[0]
+            correct += preds.eq(labels).sum().item()
+
+            loss += loss_fn(outputs, labels).item()
+
+    accuracy = round(correct / total * 100., 4)
+    loss_per_batch = round(loss / len(val_loader), 4)
+    print(f"[Val] Accuracy: {accuracy}%,"
           + f" loss per batch: {loss_per_batch}")
-    return 0, loss_per_batch
+    return accuracy, loss_per_batch
--- train.py
+++ train.py
@@ -6,11 +6,42 @@ import pandas as pd
 import torch
 from torch.autograd.profiler import record_function
 
-from cls_train import get_gradient_stats, save_train_outputs, load_train_outputs, load_model_with_grads
+def get_gradient_stats(model):
+    stats = {'parameter_i': [], 'name': [], 'mean': [], 'median': [], 'std': [], 'max': [], 'min': [], 'mean_abs': [], 'median_abs': [], 'std_abs': [], 'size_of_tensor':[]}
+    for i, (name, parameter) in enumerate(model.named_parameters()):
+        stats['parameter_i'].append(i)
+        stats['name'].append(name)
+        _grad = parameter.grad.flatten()
+        stats['mean'].append(torch.mean(_grad).item())
+        stats['median'].append(torch.median(_grad).item())
+        stats['std'].append(torch.std(_grad).item())
+        stats['max'].append(torch.max(_grad).item())
+        stats['min'].append(torch.min(_grad).item())
+        stats['mean_abs'].append(torch.mean(torch.abs(_grad)).item())
+        stats['median_abs'].append(torch.median(torch.abs(_grad)).item())
+        stats['std_abs'].append(torch.std(torch.abs(_grad)).item())
+        stats['size_of_tensor'].append(len(_grad))
+    return pd.DataFrame(stats)
 
-def train(epochs, train_loader, val_loader, model, optimizer, 
-          loss_fn, scheduler, scaler, outputs_path, save_at=1, save_grad=True, 
-          resume=False):
+def save_train_outputs(path, history, gradient_stats):
+    _dict = {'history': history,
+             'gradient_stats': gradient_stats}
+    
+    with open(os.path.join(path, 'train_outputs.pickle'), 'wb') as file:
+        pickle.dump(_dict, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_train_outputs(path):
+    with open(os.path.join(path, 'train_outputs.pickle'), 'rb') as file:
+        return pickle.load(file)
+
+def load_model_with_grads(model_path, epoch):
+    model = torch.load(os.path.join(model_path, f"model_e{epoch}.pt"))
+    grad_dict = torch.load(os.path.join(model_path, f"grad_e{epoch}.pt"))
+    for name, p in model.named_parameters():
+        p.grad = grad_dict[name]
+    return model
+
+def train(epochs, train_loader, val_loader, model, optimizer, loss_fn, scheduler, outputs_path, save_at=1, save_grad=True, resume=False):
     model_path = os.path.join(outputs_path, 'models/')
     if not os.path.exists(model_path):
         os.makedirs(model_path)
@@ -24,14 +55,13 @@ def train(epochs, train_loader, val_loader, model, optimizer,
         model.load_state_dict(state['model'])
         optimizer.load_state_dict(state['optimizer'])
         scheduler.load_state_dict(state['scheduler'])
-        scaler.load_state_dict(state['scaler'])
     else:
         starting_epoch = 1
         history = pd.DataFrame(columns=['datetime',
                                        'epoch',
-                                       'train_mAP',
+                                       'train_accuracy',
                                        'train_loss_per_batch',
-                                       'val_mAP',
+                                       'val_accuracy',
                                        'val_loss_per_batch'])
     
         gradient_stats = []
@@ -39,12 +69,12 @@ def train(epochs, train_loader, val_loader, model, optimizer,
     for epoch in range(starting_epoch, int(epochs + starting_epoch)):
         _datetime = datetime.datetime.now()
         print(f"{_datetime} Epoch {epoch} ")
-        train_map, train_loss = training_loop(optimizer, model, loss_fn, train_loader, scaler)
+        train_accuracy, train_loss = training_loop(optimizer, model, loss_fn, train_loader)
 
-        val_map, val_loss = validation_loop(model, val_loader, loss_fn)
+        val_accuracy, val_loss = validation_loop(model, val_loader, loss_fn)
         _gradient_stats = get_gradient_stats(model)
 
-        history.loc[epoch - 1] = [_datetime, epoch, train_map, train_loss, val_map, val_loss]
+        history.loc[epoch - 1] = [_datetime, epoch, train_accuracy, train_loss, val_accuracy, val_loss]
         gradient_stats.append(_gradient_stats)
 
         before_lr = optimizer.param_groups[0]["lr"]
@@ -62,8 +92,7 @@ def train(epochs, train_loader, val_loader, model, optimizer,
     state = {'epoch': int(epochs + starting_epoch),
             'model': model.state_dict(),
             'optimizer': optimizer.state_dict(),
-            'scheduler': scheduler.state_dict(),
-            'scaler': scaler.state_dict()
+            'scheduler': scheduler.state_dict()
            }
     torch.save(state, os.path.join(outputs_path, f"state.pt"))
     save_train_outputs(outputs_path, history, gradient_stats)
